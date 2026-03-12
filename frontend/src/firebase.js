fc26f761022f44f7fff21943dc6a3a447e8b8a5a
ThunderGod-54 <kinnalrish315@gmail.com>
2026-03-12 22:36:03 +0530

    hello

---
+++ frontend/src/firebase.js
@@ -0,0 +1,15 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCTddcYGsxOmtz8T7wzEFZGjpSvEe_AJV0",
+//     authDomain: "maandiconnect.firebaseapp.com",
+//     projectId: "maandiconnect",
+//     storageBucket: "maandiconnect.firebasestorage.app",
+//     messagingSenderId: "413944467279",
+//     appId: "1:413944467279:web:4bb45a49e60a78a41d3f3a"
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
